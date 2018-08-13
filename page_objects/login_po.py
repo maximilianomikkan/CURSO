f61c5eda89e24868f18097160d67d325f10bd633
Maximiliano Mikkan <maximilianomikkan@hotmail.com>
2018-08-13 10:10:01 -0300

    falla cuando cargo el form de login

--- page_objects/login_po.py
+++ page_objects/login_po.py
@@ -5,25 +5,19 @@ from selenium.webdriver.common.by import By
 
 class Login(BasePage):
     def __init__(self, driver):
-        #super(Login, self).__init__(driver)
         self.driver = driver
-        print("holaaaaaa")
-        txf_username_we = (By.ID, "username")
+
 
 
 
     def complete_and_sumbit(self, user, password):
 
-        print("ttt")
         txf_username_we = self.driver.find_element(By.XPATH, "//*[@id='username']")
-        txf_username_we = self.driver.find_element(By.ID, "username")
-
-        print("rrr")
-
+        #txf_username_we = self.driver.find_element(By.ID, "username")
+        txf_username_we.clear()
 
         txf_password_we = self.driver.find_element(By.XPATH, "//*[@id='password']")
         txf_password_we.clear()
-        print("aaaaaaaa")
 
         # cargo los text fields
         #txf_username_we.send_keys(user)
